ac12755913f9df76e55f186233b8c2fe656a72cf
Luis Santiago <luis-santiago97@hotmail.com>
2021-07-09 07:57:41 -0500

    fix: AuthUseCase added new MissingParamError

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -2,7 +2,7 @@ const { MissingParamError } = require('../../utils/errors')
 class AuthUseCase {
   async auth (email) {
     if (!email) {
-      throw MissingParamError('email')
+      throw new MissingParamError('email')
     }
   }
 }
