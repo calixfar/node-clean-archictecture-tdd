9106f8a7cd8cb2df451a24d685d711a0d14584ea
Luis Santiago <luis-santiago97@hotmail.com>
2021-07-10 18:13:59 -0500

    refactor: simplify code

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -15,14 +15,11 @@ module.exports = class AuthUseCase {
       throw new MissingParamError('password')
     }
     const user = await this.loadUserByEmailRopository.load(email)
-    if (!user) {
-      return null
+    const isValid = user && await this.encryter.compare(password, user.password)
+    if (isValid) {
+      const accessToken = await this.tokenGenerator.generate(user.id)
+      return accessToken
     }
-    const isValid = await this.encryter.compare(password, user.password)
-    if (!isValid) {
-      return null
-    }
-    const accessToken = await this.tokenGenerator.generate(user.id)
-    return accessToken
+    return null
   }
 }
